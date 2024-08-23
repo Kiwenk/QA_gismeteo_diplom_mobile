2a7acb1a2e5566ce0655347b25c50cec73eb5ffb
Katsuba Igor <kasubaigor@gmail.com>
2024-08-23 21:08:14 +0300

    q

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,80 +1,46 @@
-import allure
 import pytest
-import allure_commons
-from appium.options.android import UiAutomator2Options
-from dotenv import load_dotenv
-from selene import browser, support
-import os
 from appium import webdriver
+from dotenv import load_dotenv
+from selene import browser
+
+import config
+from utils import attach
 
 
-@pytest.fixture(autouse=True)
-def load_env():
-    load_dotenv()
-
-
-def attach_bstack_video(session_id):
-    import requests
-    bstack_session = requests.get(
-        f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
-        auth=(os.getenv('LOGIN'), os.getenv('KEY')),
-    ).json()
-    print(bstack_session)
-    video_url = bstack_session['automation_session']['video_url']
-
-    allure.attach(
-        '<html><body>'
-        '<video width="100%" height="100%" controls autoplay>'
-        f'<source src="{video_url}" type="video/mp4">'
-        '</video>'
-        '</body></html>',
-        name='video recording',
-        attachment_type=allure.attachment_type.HTML,
+def pytest_addoption(parser):
+    parser.addoption(
+        "--context",
+        default="bstack",
+        help="Specify the test context"
     )
 
 
-@pytest.fixture(autouse=True)
-def mobile_management():
-    options = UiAutomator2Options().load_capabilities({
-        'platformVersion': '10.0',
-        'deviceName': 'Google Pixel 4',
-        'app': 'bs://sample.app',
-        'bstack:options': {
-            'projectName': 'First Python project',
-            'buildName': 'browserstack-build-1',
-            'sessionName': 'BStack first_test',
-            'userName': os.getenv('LOGIN'),
-            'accessKey': os.getenv('KEY'),
-        }
-    })
+def pytest_configure(config):
+    context = config.getoption("--context")
+    env_file_path = f".env.{context}"
 
-    browser.config.driver = webdriver.Remote(
-        'http://hub.browserstack.com/wd/hub',
-        options=options
-    )
+    load_dotenv(dotenv_path=env_file_path)
 
-    browser.config.timeout = float(os.getenv('timeout', '10.0'))
 
-    browser.config._wait_decorator = support._logging.wait_with(
-        context=allure_commons._allure.StepContext
-    )
+@pytest.fixture
+def context(request):
+    return request.config.getoption("--context")
+
+
+@pytest.fixture(scope='function', autouse=True)
+def mobile_management(context):
+    options = config.to_driver_options(context=context)
+
+    browser.config.driver = webdriver.Remote(options.get_capability('remote_url'), options=options)
+    browser.config.timeout = 10.0
 
     yield
 
-    allure.attach(
-        browser.driver.get_screenshot_as_png(),
-        name='screenshot',
-        attachment_type=allure.attachment_type.PNG,
-    )
-
-    allure.attach(
-        browser.driver.page_source,
-        name='screen xml dump',
-        attachment_type=allure.attachment_type.XML,
-    )
-
+    attach.add_screenshot()
+    attach.add_xml()
     session_id = browser.driver.session_id
 
     browser.quit()
 
-    attach_bstack_video(session_id)
+    if context == 'bstack':
+        attach.add_video(session_id)
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -4,9 +4,31 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
+@allure.story("Запускаем приложение Википедии в первый раз")
+@allure.severity(allure.severity_level.CRITICAL)
+def test_getting_started():
+    with allure.step('Проверяем первую страницу'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
+            have.text('The Free Encyclopedia\n…in over 300 languages'))
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/secondaryTextView')).should(
+            have.text('We’ve found the following on your device:'))
+    with allure.step('Проверяем вторую страницу'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
+            have.text('New ways to explore'))
+    with allure.step('Проверяем третью страницу'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(
+            have.text('Reading lists with sync'))
+    with allure.step('Проверяем четвёртую страницу'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')).should(have.text('Data & Privacy'))
+
+
 @allure.story("Ищем в википедии Appium")
 def test_search():
     with step('Вводим "Appium" в поиске Википедии'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
 
@@ -20,6 +42,7 @@ def test_search():
 @allure.severity(allure.severity_level.CRITICAL)
 def test_open_first_article_DMC():
     with step('Вводим "Devil May Cry" в поиске Википедии'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Devil May Cry')
 
